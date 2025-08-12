7ae090d054e3d22ce0993010f6e866d0ce1df195
thisiscoinbirdface <142752402+thisiscoinbirdface@users.noreply.github.com>
2025-08-12 22:34:33 +0530

    added audio buses

---
+++ src/screen_options/v_box_container.gd
@@ -0,0 +1,7 @@
+extends VBoxContainer
+
+
+
+func _on_slider_music_value_changed(value: float) -> void:
+	Settings.mus_vol = value
+	pass # Replace with function body.
---
+++ src/scripts/Settings.gd
@@ -0,0 +1,12 @@
+extends Node
+
+# settings/options variables
+
+var sfx_vol = 5
+var mus_vol = 5
+var brightness = 5
+
+var font_size = 1
+var font_type = 0
+
+var scroll_speed = 1
