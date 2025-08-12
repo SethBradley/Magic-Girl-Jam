35973a3eb8019c6b675af1c3332f29e6fc68bf4a
thisiscoinbirdface <142752402+thisiscoinbirdface@users.noreply.github.com>
2025-08-12 22:48:59 +0530

    audio sliders work.

--- src/screen_options/v_box_container.gd
+++ src/screen_options/v_box_container.gd
@@ -1,7 +1,22 @@
 extends VBoxContainer
 
+var bus_index: int
 
+# audio functions
+# music and sfx play through their own audio buses
+# the sliders set the bus volume directly
+# the sliders set Settings.variable as well (but this setting isn't used) 
 
+# music volume
 func _on_slider_music_value_changed(value: float) -> void:
+	bus_index = AudioServer.get_bus_index("music")
 	Settings.mus_vol = value
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value)) # make this a set_audio_vol() function?
+	pass # Replace with function body.
+
+# sfx volume
+func _on_slider_sfx_value_changed(value: float) -> void:
+	bus_index = AudioServer.get_bus_index("sfx")
+	Settings.mus_vol = value
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value)) # make this a set_sfx_vol() function?
 	pass # Replace with function body.
--- src/scripts/Settings.gd
+++ src/scripts/Settings.gd
@@ -2,9 +2,9 @@ extends Node
 
 # settings/options variables
 
-var sfx_vol = 5
-var mus_vol = 5
-var brightness = 5
+var sfx_vol = 10
+var mus_vol = 10
+var brightness = 10
 
 var font_size = 1
 var font_type = 0
