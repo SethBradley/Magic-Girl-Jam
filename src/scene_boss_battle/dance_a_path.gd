042ee15cd67e04660f87953ea2dc65f4cd1345e8
thisiscoinbirdface <142752402+thisiscoinbirdface@users.noreply.github.com>
2025-08-15 23:09:14 +0530

    added generic movement

---
+++ src/scene_boss_battle/dance_a_path.gd
@@ -0,0 +1,11 @@
+extends PathFollow2D
+
+var speed = 0.5
+
+func _process(delta):
+	loop_movement(delta)
+
+func loop_movement(delta):
+	progress_ratio += delta * speed
+	
+	
--- src/screen_start/start_screen.gd
+++ src/screen_start/start_screen.gd
@@ -1,12 +1,13 @@
 extends Control
 
 func _on_start_pressed():
-	pass # Replace with function body.
-
-
+	get_tree().change_scene_to_file("res://src/scene_boss_battle/boss_battle.tscn")
+	
 func _on_options_pressed():
 	get_tree().change_scene_to_file("res://src/screen_options/options_menu.tscn")
 
-
 func _on_exit_pressed():
 	get_tree().quit()
+
+func _on_boss_battle_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/scene_boss_battle/boss_battle.tscn")
